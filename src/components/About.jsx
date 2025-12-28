f57f78af7443d975e83608b0ebb8f7d4f2fe7222
brandolanchez <lanchez456@gmail.com>
2025-12-27 22:18:54 -0400

    Descripción de lo que cambiaste

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className="max-w-4xl mx-auto text-center">
         
         <Carousel />
-        <h2 className="font-heading text-xl md:text-5xl my-8"><span className="text-fortis-gold">¿Qué es Fortis Workout?</span></h2>
+        <h2 className="font-heading text-4xl md:text-5xl my-8"><span className="text-fortis-gold">¿Qué es Fortis Workout?</span></h2>
         <p className="mt-1 text-gray-300 text-4xl leading-relaxed">
           Somos un club de calistenia que entrena en las barras del Parque Vereda del Lago en Maracaibo, dedicado a desarrollar fuerza, control corporal y mentalidad de alto rendimiento.
           No se trata solo de hacer dominadas: se trata de forjar disciplina, enfoque y resiliencia en comunidad.
